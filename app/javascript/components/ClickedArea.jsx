ad825f88400bca972a88865e94e676b76aa0c239
juroga_new <juanjrgast@gmail.com>
2023-12-28 18:03:33 +0100

    Update styling for ClickedArea component

--- app/javascript/components/ClickedArea.jsx
+++ app/javascript/components/ClickedArea.jsx
@@ -2,17 +2,19 @@ import React from "react";
 
 const ClickedArea = ({ x, y }) => {
     const style = {
-      left: `${x}px`,
-      top: `${y}px`,
-      // Additional styling
+      left: `${x - 50}px`,
+      top: `${y + 100}px`,
+      width: '100px',
+      height: '100px',
+      border: '6px dashed purple',
+      backgroundColor: 'rgba(128, 128, 128, 0.5)',
+      position: 'absolute',
     };
   
     return (
-        <div className="clicked-area" style={style}>;
-            <h2>Clicked Area</h2>
+        <div className="clicked-area" style={style}>
         </div>
     );
 };
 
 export default ClickedArea;
-  
