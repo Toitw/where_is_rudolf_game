78cbc98995181533b330a393c2163d2b12536ffa
juroga_new <juanjrgast@gmail.com>
2024-01-13 14:05:59 +0100

    Update ClickedArea style

--- app/javascript/components/ClickedArea.jsx
+++ app/javascript/components/ClickedArea.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const ClickedArea = ({ x, y }) => {
     const style = {
-      left: `${x-25}px`,
-      top: `${y-25}px`,
-      width: '50px',
-      height: '50px',
+      left: `${x-20}px`,
+      top: `${y-20}px`,
+      width: '40px',
+      height: '40px',
       border: '6px dashed purple',
       backgroundColor: 'rgba(128, 128, 128, 0.5)',
       position: 'absolute',
