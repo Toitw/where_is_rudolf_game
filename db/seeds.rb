2555ee03f2578988f2d9270995b6b8d0b1138b7e
juroga_new <juanjrgast@gmail.com>
2024-01-14 16:20:12 +0100

    Update character coordinates in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-Character.create(name: 'Snowflake Reindeer', x_coordinate: 1064, y_coordinate: 715.281)
-Character.create(name: 'Zen Reindeer', x_coordinate: 47, y_coordinate: 747.281)
-Character.create(name: 'Proud Reindeer', x_coordinate: 297, y_coordinate: 752.281)
-
+Character.create(name: 'Snowflake Reindeer', x_coordinate: 89.16090247852057, y_coordinate: 51.08788760561106)
+Character.create(name: 'Zen Reindeer', x_coordinate: 3.8062284942804934, y_coordinate: 53.76462554900724)
+Character.create(name: 'Proud Reindeer', x_coordinate: 24.550173788109184, y_coordinate: 53.84618821481209)
